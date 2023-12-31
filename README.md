25128ccab19d90451e4b3af2fa839a9256095489
Diego Zavala <diego.zavala.t@gmail.com>
2023-12-31 08:18:01 -0500

    Aplicando la ETL para el proceso de Admision PEA-DataSciente - primer avance

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # PASOS PARA CONFIGURAR EL PROYECTO
 
 * 1. En la raiz del proyecto crear el entorno virtual ejecutando el comando: virtualenv env
-* 2. Para activar el entorno creado, en un terminal bash ejecutamos el siguiente comando: source env/bin/activate(Mac)
+* 2. Para activar el entorno creado, en un terminal bash ejecutamos el siguiente comando: env/bin/activate(Mac)
 * 3. Necesitamos instalar la librerias pandas y sqlalchemy: 
     * 3.1 Dentro del entorno virtual, ejecutar el siguiente comando: pip install pandas
     * 3.2 Dentro del entorno virtual, ejecutar el siguiente comando: pip install sqlalchemy
 * 4. Antes de ejecutar el proyecto por primera vez, necesitamos crear la siguiente estrutura de carpetas en la raiz del proyecto:
     * 4.1 server_inputs: donde alojaremos el archivo de entrada que procesaremos
-    * 4.2 server_outputs: donde alojaremos los archivos de salida del proyecto: clientes.csv y deudas.csv
-    * 4.3 database: donde se creara automaticamente la base de datos con la data de deudas previamente procesada.
+    * 4.2 database: Debemos encargarnos de crear un contenedor de BD MySQL desplegado localmente. Para ello usaremos este comando:
+        * docker run -d --rm --name mysql -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 mysql:8.0
+        * De igual manera debemos asegurarnos que exista una database con el nombre: pea-datascience
+        * Asimismo debemos instalar el connector para sqlalchemy y MySQL, para ello ejecutaremos este comando: pip install mysql-connector-python
+    
 
 > **IMPORTANTE:** Debemos asegurarnos que el Python Interpreter con el que estamos trabajando, sea el del virtualenv que hemos creado.
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 import sqlalchemy as sa
 
 
-FILENAME = 'file.ope'
+FILENAME = 'admision-univ-121223.csv'
 INPUT_PATH = 'server_inputs/'
 OUTPUT_PATH = 'server_outputs/'
 
-DB_NAME = 'db_sbs.sqlite'
-DB_LOCATION = f'sqlite:///database/{DB_NAME}'
+DB_NAME = 'pea-datascience'
+DB_LOCATION = f'mysql+mysqlconnector://root:root@localhost:3306/{DB_NAME}'
 
 COL_DEUDAS = {
         'CodigoSBS': 'Cod_SBS',
@@ -21,72 +21,79 @@ COL_DEUDAS = {
     }
 
 
-def extract(ind_clientes):
-    df_extracted = pd.read_csv(INPUT_PATH + FILENAME)
-    df_extracted['Value'] = df_extracted['Field_1'].str[0]
-    df_respuesta = df_extracted[df_extracted['Value'] == ('1' if ind_clientes else '2')].reset_index(drop=True).copy()
-    return df_respuesta
+def extract():
+    df_admision = pd.read_csv(INPUT_PATH + FILENAME, encoding="ISO-8859-1")
+    return df_admision
 
 
-def transform(df, ind_clientes):
-    df_resultado = transform_clientes(df) if ind_clientes else transform_deudas(df)
+def transform(df):
+    df_resultado = pd.DataFrame()
+    # Llenamos los campos de clientes
+    df_resultado['NombreAlumno'] = df['NOMBRES'].astype('str')
+    df_resultado['CodSed'] = df['COD_SED'].astype('str')
+    df_resultado['SemApe'] = df['SEM_APE'].astype('str')
+    df_resultado['CodEsp'] = df['COD_ESP'].astype('str')
+    df_resultado['FechaExamen'] = df['FECH_EXAM'].astype('str')
+    df_resultado['HoraExamen'] = df['HORA_EXAM'].astype('str')
+    df_resultado['Sexo'] = df['SEXO'].astype('str')
+    df_resultado['FechaNacimiento'] = df['FNACIMIEN'].astype('str')
+    df_resultado['DiaNacimiento'] = df['DIANACIMI'].astype('str')
+    df_resultado['MesNacimiento'] = df['MESNACIMI'].astype('str')
+    df_resultado['AnoNacimiento'] = df['ANNONACIM'].astype('str')
+    df_resultado['CodigoModalidad'] = df['CODMOD'].astype('str')
+    df_resultado['Modalidad'] = df['MODALIDAD'].astype('str')
+    df_resultado['Sede'] = df['SEDE'].astype('str')
+    df_resultado['Semestre'] = df['SEMESTRE'].astype('str')
+    df_resultado['Especialidad'] = df['ESPECIALIDAD'].astype('str')
+    df_resultado['CodFac'] = df['CODFAC'].astype('str')
+    df_resultado['Facultad'] = df['FACULTAD'].astype('str')
+    df_resultado['Puntaje'] = df['PUNTAJE'].astype('str')
+    df_resultado['Merito'] = df['MERITO'].astype('str')
+    df_resultado['EstadoPost'] = df['ESTADOPOST'].astype('str')
+    df_resultado['Observacion'] = df['OBSERVACION'].astype('str')
+    df_resultado['Resultado'] = df['RESULTADO'].astype('str')
+    df_resultado['NombreColegio'] = df['NOMCOLEGI'].astype('str')
+    df_resultado['PaisColegio'] = df['PAISCOL'].astype('str')
+    df_resultado['UbigeoColegio'] = df['UBIGEOCOLE'].astype('str')
+    df_resultado['DepartamentoColegio'] = df['DEPCOL'].astype('str')
+    df_resultado['ProvinciaColegio'] = df['PROCOL'].astype('str')
+    df_resultado['DistritoColegio'] = df['DISCOL'].astype('str')
+    df_resultado['UbigeoColegio2'] = df['UBICOLEGI'].astype('str')
+    df_resultado['DireccionColegio'] = df['DIRCOL'].astype('str')
+    df_resultado['CodigoTipoColegio'] = df['CODTIPCO'].astype('str')
+    df_resultado['TipoColegio'] = df['TIPCOLEGI'].astype('str')
+    df_resultado['EgresoColegio'] = df['EGRESOCOL'].astype('str')
+    df_resultado['CodigoPreparatoria'] = df['CODPREPAR'].astype('str')
+    df_resultado['DescripcionPreparatoria'] = df['DESPREPAR'].astype('str')
+    df_resultado['Departamento'] = df['DEPARTAMENTO'].astype('str')
+    df_resultado['Provincia'] = df['PROVINCIA'].astype('str')
+    df_resultado['Distrito'] = df['DISTRITO'].astype('str')
+    df_resultado['UbigeoResidencia'] = df['UBIGEORESIDENCIA'].astype('str')
+    df_resultado['CodigoEstadoCivil'] = df['CODESTCIV'].astype('str')
+    df_resultado['EstadoCivil'] = df['ESTADOCIVI'].astype('str')
+    df_resultado['Trabaja'] = df['TRABAJA'].astype('str')
+    df_resultado['CodigoNivelEducacion'] = df['CODNIVEDU'].astype('str')
+    df_resultado['DescripcionNivelEducacion'] = df['DESNIVEDU'].astype('str')
+    df_resultado['TipoEnc'] = df['TIP_ENC'].astype('str')
+    df_resultado['CodigoPais'] = df['CODPAIS'].astype('str')
+    df_resultado['UniversidadProcedencia'] = df['UNIVERSIDADPROCEDENCIA'].astype('str')
+    df_resultado['FacultadProcedencia'] = df['FACULTADPROCEDENCIA'].astype('str')
+    df_resultado['EscuelaProcedencia'] = df['ESCUELAPROCEDENCIA'].astype('str')
+    df_resultado['DesPais'] = df['DESPAIS'].astype('str')
+    df_resultado['Grado1'] = df['GRADO1'].astype('str')
+    df_resultado['Col1'] = df['COL1'].astype('str')
+    df_resultado['Grado2'] = df['GRADO2'].astype('str')
+    df_resultado['Col2'] = df['COL2'].astype('str')
+    df_resultado['Grado3'] = df['GRADO3'].astype('str')
+    df_resultado['Col3'] = df['COL3'].astype('str')
+    df_resultado['ID'] = df['ID'].astype('str')
+    
+    print(df_resultado)
+    
     return df_resultado
 
-def transform_clientes(df):
-    df_clientes = pd.DataFrame()
-    # Llenamos los campos de clientes
-    df_clientes['SBSCodigoCliente'] = df['Field_1'].astype('str').str.split('|')[0]
-    df_clientes['SBSFechaReporte'] = df['Field_1'].astype('str').str.split('|')[1]
-    df_clientes['SBSTipoDocumentoT'] = df['Field_1'].astype('str').str.split('|')[2]
-    df_clientes['SBSRucCliente'] = df['Field_1'].astype('str').str.split('|')[3]
-    df_clientes['SBSTipoDocumento'] = df['Field_1'].astype('str').str.split('|')[4]
-    df_clientes['SBSNumeroDocumento'] = df['Field_1'].astype('str').str.split('|')[5]
-    df_clientes['SBSTipoPer'] = df['Field_1'].astype('str').str.split('|')[6]
-    df_clientes['SBSTipoEmpresa'] = df['Field_1'].astype('str').str.split('|')[7]
-    df_clientes['SBSNumeroEntidad'] = df['Field_1'].astype('str').str.split('|')[8]
-    df_clientes['SBSSalNor'] = df['Field_1'].astype('str').str.split('|')[9]
-    df_clientes['SBSSalCPP'] = df['Field_1'].astype('str').str.split('|')[10]
-    df_clientes['SBSSalDEF'] = df['Field_1'].astype('str').str.split('|')[11]
-    df_clientes['SBSSalDUD'] = df['Field_1'].astype('str').str.split('|')[12]
-    df_clientes['SBSSalAPER'] = df['Field_1'].astype('str').str.split('|')[13]
-    df_clientes['SBSAPEPAT'] = df['Field_1'].astype('str').str.split('|')[14]
-    df_clientes['SBSAPEMAT'] = df['Field_1'].astype('str').str.split('|')[15]
-    df_clientes['SBSAPECAS'] = df['Field_1'].astype('str').str.split('|')[16]
-    df_clientes['SBSNOMCLI'] = df['Field_1'].astype('str').str.split('|')[17]
-    df_clientes['SBSNOMCLI2'] = df['Field_1'].astype('str').str.split('|')[18]
-    print(df_clientes)
-    
-    return df_clientes
 
-def transform_deudas(df):
-    df_deuda = pd.DataFrame()
-    # Llenamos los campos de deudas
-    df_deuda['CodigoSBS'] = df['Field_1'].str[1:11]
-    df_deuda['CodigoEmpresa'] = df['Field_1'].str[11:16]
-    df_deuda['TipoCredito'] = df['Field_1'].str[16:18]
-    df_deuda['Nivel2'] = df['Field_1'].str[18:20]
-    df_deuda['Moneda'] = df['Field_1'].str[20:21]
-    df_deuda['SubCodigoCuenta'] = df['Field_1'].str[21:32]
-    df_deuda['Condicion'] = df['Field_1'].str[32:38]
-    df_deuda['ValorSaldo'] = df['Field_1'].str[38:42]
-    df_deuda['ClasificacionDeuda'] = df['Field_1'].str[42:43]
-    df_deuda['CodigoCuenta'] = df_deuda['Nivel2'] + df_deuda['Moneda'] + df_deuda['SubCodigoCuenta']
-    
-    # Renombramos los nombres de las columnas
-    df_deuda.rename(columns=COL_DEUDAS, inplace=True)
-    print(df_deuda)
-    
-    return df_deuda
-
-def load(df_data, filename):
-    print(f'===> Loading {filename}')
-    path_output = os.path.join(OUTPUT_PATH, filename)
-    df_data.to_csv(path_output)
-
-
-def load_db(df):
-    # Eliminamos la columna SubCodigoCuenta para que matche el df y la BD a definir
-    df.drop('SubCodigoCuenta', axis=1, inplace=True)
+def load(df):
     
     # Creamos la conexion
     engine = sa.create_engine(DB_LOCATION)
@@ -95,41 +102,83 @@ def load_db(df):
     with engine.connect() as con:
         # Creamos la tabla
         sql_query = '''
-            CREATE TABLE IF NOT EXISTS deudas_sbs(
-                Cod_SBS VARCHAR(200), 
-                Cod_Emp VARCHAR(200), 
-                Tip_Credit VARCHAR(200), 
-                Nivel2 VARCHAR(200), 
-                Moneda VARCHAR(200), 
-                Condicion VARCHAR(200), 
-                Val_Saldo VARCHAR(200), 
-                Clasif_Deu VARCHAR(200), 
-                Cod_Cuenta VARCHAR(200)
+            CREATE TABLE IF NOT EXISTS ProcesoAdmision(
+                NombreAlumno VARCHAR(200), 
+                CodSed VARCHAR(200), 
+                SemApe VARCHAR(200), 
+                CodEsp VARCHAR(200), 
+                FechaExamen VARCHAR(200), 
+                HoraExamen VARCHAR(200), 
+                Sexo VARCHAR(200), 
+                FechaNacimiento VARCHAR(200),
+                DiaNacimiento VARCHAR(200),
+                MesNacimiento VARCHAR(200),
+                AnoNacimiento VARCHAR(200),
+                CodigoModalidad VARCHAR(200),
+                Modalidad VARCHAR(200),
+                Sede VARCHAR(200),
+                Semestre VARCHAR(200),
+                Especialidad VARCHAR(200),
+                CodFac VARCHAR(200),
+                Facultad VARCHAR(200),
+                Puntaje VARCHAR(200),
+                Merito VARCHAR(200),
+                EstadoPost VARCHAR(200),
+                Observacion VARCHAR(200),
+                Resultado VARCHAR(200),
+                NombreColegio VARCHAR(200),
+                PaisColegio VARCHAR(200),
+                UbigeoColegio VARCHAR(200),
+                DepartamentoColegio VARCHAR(200),
+                ProvinciaColegio VARCHAR(200),
+                DistritoColegio VARCHAR(200),
+                UbigeoColegio2 VARCHAR(200),
+                DireccionColegio VARCHAR(200),
+                CodigoTipoColegio VARCHAR(200),
+                TipoColegio VARCHAR(200),
+                EgresoColegio VARCHAR(200),
+                CodigoPreparatoria VARCHAR(200),
+                DescripcionPreparatoria VARCHAR(200),
+                Departamento VARCHAR(200),
+                Provincia VARCHAR(200),
+                Distrito VARCHAR(200),
+                UbigeoResidencia VARCHAR(200),
+                CodigoEstadoCivil VARCHAR(200),
+                EstadoCivil VARCHAR(200),
+                Trabaja VARCHAR(200),
+                CodigoNivelEducacion VARCHAR(200),
+                DescripcionNivelEducacion VARCHAR(200),
+                TipoEnc VARCHAR(200),
+                CodigoPais VARCHAR(200),
+                UniversidadProcedencia VARCHAR(200),
+                FacultadProcedencia VARCHAR(200),
+                EscuelaProcedencia VARCHAR(200),
+                DesPais VARCHAR(200),
+                Grado1 VARCHAR(200),
+                Col1 VARCHAR(200),
+                Grado2 VARCHAR(200),
+                Col2 VARCHAR(200),
+                Grado3 VARCHAR(200),
+                Col3 VARCHAR(200),
+                ID VARCHAR(200)
             )
         '''
         sql_query = sa.sql.text(sql_query)
         con.execute(sql_query)
         print('===> Tabla creada satisfactoriamente')
 
-    df.to_sql('deudas_sbs', engine, index=False, if_exists='append')
+    df.to_sql('ProcesoAdmision', engine, index=False, if_exists='append')
 
     print('===> Los datos fueron cargados satisfactoriamente')
-    
 
 
 if __name__ == '__main__':
-    # Extract Clientes y Deudas
-    df_clientes = extract(True)
-    df_deudas = extract(False)
+    # Extraemos la informacion del ultimo proceso de admision
+    df_admision = extract()
 
     # Transformamos Clientes y Deudas al formato solicitado
-    df_transformed_clientes = transform(df_clientes, True)
-    df_transformed_deudas = transform(df_deudas, False)
-
-    # Load
-    load(df_transformed_clientes, 'clientes.csv')
-    load(df_transformed_deudas, 'deudas.csv')
+    df_alumnos_admision = transform(df_admision)
 
     # Load to DB
-    load_db(df_transformed_deudas)
+    load(df_alumnos_admision)
 
